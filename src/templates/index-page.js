992e28f321f143743d39d74461a62608cf13edca
Desi-Bakar <desybakar12@gmail.com>
2025-06-02 16:47:51 +0700

    update

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -28,22 +28,27 @@ export const IndexPageTemplate = ({
             <div className="columns">
               <div className="column is-10 is-offset-1">
                 <div className="content">
-                  <div className="content">
-                    <div className="tile">
-                      <h1 className="title">{mainpitch.title}</h1>
-                    </div>
-                    <div className="tile">
-                      <h3 className="subtitle">{mainpitch.description}</h3>
-                    </div>
-                  </div>
+                  
+                  
                   <div className="columns">
                     <div className="column is-12">
                       <h3 className="has-text-weight-semibold is-size-2">
                         {heading}
                       </h3>
-                      <p>{description}</p>
+                      <h3>{description}</h3>
                     </div>
                   </div>
+
+                  <div className="content">
+                    <div className="tile">
+                      <h1 className="title">{mainpitch.title}</h1>
+                    </div>
+                    <div className="tile">
+                      <p className="subtitle">{mainpitch.description}</p>
+                    </div>
+                  </div>
+
+
                   <Features gridItems={intro.blurbs} />
                   <div className="columns">
                     <div className="column is-12 has-text-centered">

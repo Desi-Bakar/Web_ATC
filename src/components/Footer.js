321487010e1ddc5bdf5ae7d6c94894b871183724
Desi Bakar <desybakar12@gmail.com>
2025-08-06 04:49:45 +0000

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -69,7 +69,7 @@ const Footer = () => {
                 display: "inline-block",
                 backgroundColor: "#ffffff",
                 padding: "0.5rem",
-                borderRadius: "0",
+                borderRadius: "15px",
                 boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
               }}
             >

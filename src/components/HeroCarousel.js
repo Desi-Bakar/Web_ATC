4e497bdd1b08e80e38d795c493498decc6cc50ef
Desi Bakar <desybakar12@gmail.com>
2025-08-07 13:28:05 +0000

    update

--- src/components/HeroCarousel.js
+++ src/components/HeroCarousel.js
@@ -1,83 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+import "swiper/css";
+
+import "swiper/css/pagination";
 
 const images = [
-  "/img/DESIGNN.png", // Ganti dengan path gambar kamu
+  "/img/DESIGNN.png",
   "/img/DESIGNN1.png",
   "/img/aretanet.png",
 ];
 
 const HeroCarousel = () => {
-  const [current, setCurrent] = useState(0);
-
-  // Auto slide tiap 5 detik
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrent((prev) => (prev + 1) % images.length);
-    }, 5000); // Ganti ke 10000 untuk 10 detik
-    return () => clearInterval(interval);
-  }, []);
-
-  const nextSlide = () => {
-    setCurrent((prev) => (prev + 1) % images.length);
-  };
-
-  const prevSlide = () => {
-    setCurrent((prev) => (prev - 1 + images.length) % images.length);
-  };
-
   return (
-    <div style={{ position: "relative", overflow: "hidden" }}>
-      {images.map((src, index) => (
-        <img
-          key={index}
-          src={src}
-          alt={`Slide ${index}`}
-          style={{
-            display: index === current ? "block" : "none",
-            width: "1336px",
-            height: "445px",
-            maxHeight: "445px",
-            objectFit: "cover",
-          }}
-        />
-      ))}
-
-      {/* Tombol navigasi */}
-      <button
-        onClick={prevSlide}
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: "10px",
-          transform: "translateY(-50%)",
-          background: "rgba(0,0,0,0.5)",
-          color: "#fff",
-          border: "none",
-          borderRadius: "50%",
-          padding: "10px",
-          cursor: "pointer",
-        }}
+     <div
+      style={{
+        maxWidth: "1280px",
+        height: "360px",
+        margin: "0 auto", // center secara horizontal
+        borderRadius: "16px",
+        overflow: "hidden", // untuk rounded corners
+      }}
+    >
+      <Swiper
+        modules={[Autoplay, Pagination]}
+        spaceBetween={0}
+        slidesPerView={1}
+        loop={true}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        style={{ height: "100%", width: "100%" }}
       >
-        ‹
-      </button>
-
-      <button
-        onClick={nextSlide}
-        style={{
-          position: "absolute",
-          top: "50%",
-          right: "10px",
-          transform: "translateY(-50%)",
-          background: "rgba(0,0,0,0.5)",
-          color: "#fff",
-          border: "none",
-          borderRadius: "50%",
-          padding: "10px",
-          cursor: "pointer",
-        }}
-      >
-        ›
-      </button>
+        {images.map((src, index) => (
+          <SwiperSlide key={index}>
+            <img
+              src={src}
+              alt={`Slide ${index}`}
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+              }}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };

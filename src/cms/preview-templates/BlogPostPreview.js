cf35e024ed0e38cf2f025bfefcd89a7e39f4f70c
Taqi25-ai <anindrataqi1@gmail.com>
2025-09-02 08:07:19 +0000

    update

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -1,109 +1,25 @@
+// src/cms/preview-templates/BlogPostPreview.js
 import React from "react"
 import PropTypes from "prop-types"
-import { graphql } from "gatsby"
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import Layout from "../components/Layout"
-import Content, { HTMLContent } from "../components/Content"
+import BlogPostTemplate from "../../templates/blog-post"
 
-export const BlogPostTemplate = ({
-  content,
-  contentComponent,
-  description,
-  tags,
-  title,
-  featuredimage,
-  helmet,
-}) => {
-  const PostContent = contentComponent || Content
-  const image = getImage(featuredimage)
-
+const BlogPostPreview = ({ entry, widgetFor }) => {
   return (
-    <section className="section">
-      {helmet || ""}
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-primary is-bold-light">
-              {title}
-            </h1>
-            <p className="subtitle is-size-5">{description}</p>
-
-            {image && (
-              <div className="featured-image mb-4">
-                <GatsbyImage image={image} alt={title} />
-              </div>
-            )}
-
-            <PostContent content={content} />
-
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className="taglist">
-                  {tags.map((tag) => (
-                    <li key={tag}>{tag}</li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
-          </div>
-        </div>
-      </div>
-    </section>
+    <BlogPostTemplate
+      content={widgetFor("body")}
+      description={entry.getIn(["data", "description"])}
+      tags={entry.getIn(["data", "tags"])}
+      title={entry.getIn(["data", "title"])}
+      // 🚫 jangan kirim featuredimage biar gambar kanan ga muncul di preview
+    />
   )
 }
 
-BlogPostTemplate.propTypes = {
-  content: PropTypes.node.isRequired,
-  contentComponent: PropTypes.func,
-  description: PropTypes.string,
-  tags: PropTypes.array,
-  title: PropTypes.string,
-  featuredimage: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  helmet: PropTypes.object,
-}
-
-const BlogPost = ({ data }) => {
-  const { markdownRemark: post } = data
-
-  return (
-    <Layout>
-      <BlogPostTemplate
-        content={post.html}
-        contentComponent={HTMLContent}
-        description={post.frontmatter.description}
-        tags={post.frontmatter.tags}
-        title={post.frontmatter.title}
-        featuredimage={post.frontmatter.featuredimage}
-      />
-    </Layout>
-  )
-}
-
-BlogPost.propTypes = {
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.object,
+BlogPostPreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
   }),
+  widgetFor: PropTypes.func,
 }
 
-export default BlogPost
-
-export const pageQuery = graphql`
-  query BlogPostByID($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      id
-      html
-      frontmatter {
-        date(formatString: "MMMM DD, YYYY")
-        title
-        description
-        tags
-        featuredimage {
-          childImageSharp {
-            gatsbyImageData(width: 1024, quality: 90, layout: CONSTRAINED)
-          }
-        }
-      }
-    }
-  }
-`
+export default BlogPostPreview

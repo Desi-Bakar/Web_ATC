e89e5a829b1ca195fdad9140b917f3b5bad7634e
Desy Bakar <desybakar12@gmail.com>
2026-02-04 17:52:57 +0000

    tes

--- src/components/BottomFullWidthImage.js
+++ src/components/BottomFullWidthImage.js
@@ -5,53 +5,54 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image";
 const BottomFullWidthImage = ({ image, title, imgPosition = "center" }) => {
   const img = getImage(image) || image;
 
-  return (
-    <section
-      style={{
-        margin: 0,
-        padding: 0,
-        backgroundColor: "#0057b8",
-        lineHeight: 0, // hilangin jarak ekstra di bawah
-      }}
-    >
-      {img ? (
-        <GatsbyImage
-          image={img}
-          alt={title || ""}
-          style={{
-            width: "100%",
-            height: "100%",
-            margin: 0,
-          }}
-          imgStyle={{
-            objectFit: "cover",
-            objectPosition: imgPosition,
-            margin: 0,
-          }}
-        />
-      ) : (
-        <div
-          style={{
-            backgroundColor: "#0057b8",
-            height: "400px",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            color: "#fff",
-            margin: 0,
-          }}
-        >
-          No image available
-        </div>
-      )}
-    </section>
-  );
-};
+    return (
+        <section
+              style={{
+                      margin: 0,
+                              padding: 0,
+                                      backgroundColor: "#e78311",
+                                              lineHeight: 0, // hilangin jarak ekstra di bawah
+                                                    }}
+                                                        >
+                                                              {img ? (
+                                                                      <GatsbyImage
+                                                                                image={img}
+                                                                                          alt={title || ""}
+                                                                                                    style={{
+                                                                                                                width: "100%",
+                                                                                                                            height: "100%",
+                                                                                                                                        margin: 0,
+                                                                                                                                                  }}
+                                                                                                                                                            imgStyle={{
+                                                                                                                                                                        objectFit: "cover",
+                                                                                                                                                                                    objectPosition: imgPosition,
+                                                                                                                                                                                                margin: 0,
+                                                                                                                                                                                                          }}
+                                                                                                                                                                                                                  />
+                                                                                                                                                                                                                        ) : (
+                                                                                                                                                                                                                                <div
+                                                                                                                                                                                                                                          style={{
+                                                                                                                                                                                                                                                      backgroundColor: "#0057b8",
+                                                                                                                                                                                                                                                                  height: "400px",
+                                                                                                                                                                                                                                                                              display: "flex",
+                                                                                                                                                                                                                                                                                          alignItems: "center",
+                                                                                                                                                                                                                                                                                                      justifyContent: "center",
+                                                                                                                                                                                                                                                                                                                  color: "#fff",
+                                                                                                                                                                                                                                                                                                                              margin: 0,
+                                                                                                                                                                                                                                                                                                                                        }}
+                                                                                                                                                                                                                                                                                                                                                >
+                                                                                                                                                                                                                                                                                                                                                          No image available
+                                                                                                                                                                                                                                                                                                                                                                  </div>
+                                                                                                                                                                                                                                                                                                                                                                        )}
+                                                                                                                                                                                                                                                                                                                                                                            </section>
+                                                                                                                                                                                                                                                                                                                                                                              );
+                                                                                                                                                                                                                                                                                                                                                                              };
 
-BottomFullWidthImage.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  title: PropTypes.string,
-  imgPosition: PropTypes.string,
-};
+                                                                                                                                                                                                                                                                                                                                                                              BottomFullWidthImage.propTypes = {
+                                                                                                                                                                                                                                                                                                                                                                                image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+                                                                                                                                                                                                                                                                                                                                                                                  title: PropTypes.string,
+                                                                                                                                                                                                                                                                                                                                                                                    imgPosition: PropTypes.string,
+                                                                                                                                                                                                                                                                                                                                                                                    };
 
-export default BottomFullWidthImage;
+                                                                                                                                                                                                                                                                                                                                                                                    export default BottomFullWidthImage;
+                                                                                                                                                                                                                                                                                                                                                                                    
